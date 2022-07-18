cbad64db85204f35084a42fa4e723d547ce92776
EC2 Default User <ec2-user@ip-172-31-25-12.ap-southeast-1.compute.internal>
2022-07-18 13:28:09 +0000

    change2

--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 from app import app
 with app.test_client() as c:
     response = c.get('/')
-    assert response.data == b'Hello Python!'
+    assert response.data == b'Hello Python app!'
     assert response.status_code == 200
